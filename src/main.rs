b3b103b1fd9269be7a724fdb00eab070ce10fb00
thal0x <thal0x.eth@gmail.com>
2022-07-01 05:41:21 -0500

    complete chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+
+    run(listener, connection_pool)?.await
 }
